2745b5525cfc49229768bd39e8b1810906fbc1e6
seoyoenkk68 <82437181+seoyoenkk68@users.noreply.github.com>
2021-09-03 01:39:43 +0900

    Delete docs directory

--- docs/fullpage.js
+++
@@ -1,7 +0,0 @@
-new fullpage('#fullpage', {
-    responsiveWidth:600,
-    /* scrollOverflow:true, */
-    navigation:true,
-    navigationPosition: 'left',
-    anchors: ['hero', 'overView', 'readMore', 'skills', 'profile']
-  });
